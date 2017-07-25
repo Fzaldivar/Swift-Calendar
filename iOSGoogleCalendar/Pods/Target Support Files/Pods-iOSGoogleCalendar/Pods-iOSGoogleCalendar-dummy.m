6c73b51a61f12d55d70e783438f8b2c54aea1e56
Fernando Zaldívar Coto <fernandozaldivarcoto@gmail.com>
2017-07-24 21:22:28 -0600

    Add pods to project

---
+++ iOSGoogleCalendar/Pods/Target Support Files/Pods-iOSGoogleCalendar/Pods-iOSGoogleCalendar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_iOSGoogleCalendar : NSObject
+@end
+@implementation PodsDummy_Pods_iOSGoogleCalendar
+@end
---
+++ iOSGoogleCalendar/Pods/Target Support Files/Pods-iOSGoogleCalendar/Pods-iOSGoogleCalendar-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_iOSGoogleCalendarVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_iOSGoogleCalendarVersionString[];
+
